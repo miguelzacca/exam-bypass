dca4e575252f511f03385f2ac6ccd298a358d456
Miguel Zacca <miguel_zacca@estudante.sesisenai.org.br>
2024-08-16 11:08:59 -0300

    First commit

---
+++ xss.js
@@ -0,0 +1,58 @@
+function _0x34d7(_0x9d67f9, _0x3ff3e7) {
+  var _0x3861a0 = _0x3861()
+  return (
+    (_0x34d7 = function (_0x34d7a9, _0x5ed9f1) {
+      _0x34d7a9 = _0x34d7a9 - 0x186
+      var _0x41e0ee = _0x3861a0[_0x34d7a9]
+      return _0x41e0ee
+    }),
+    _0x34d7(_0x9d67f9, _0x3ff3e7)
+  )
+}
+function _0x3861() {
+  var _0x411b82 = [
+    '45NVLYRu',
+    '1060CDpeIU',
+    '4397766sRVyMP',
+    '6472879kSydEW',
+    '3135714GGoAgX',
+    '52085gpMcbE',
+    '4009167bhkRuy',
+    '8FturLR',
+    '6BfOZRY',
+    '1425942VxPLmU',
+    '293144ihJzBJ',
+  ]
+  _0x3861 = function () {
+    return _0x411b82
+  }
+  return _0x3861()
+}
+;(function (_0x4029ef, _0x189bd8) {
+  var _0x5dfd4a = _0x34d7,
+    _0x314b3c = _0x4029ef()
+  while (!![]) {
+    try {
+      var _0x1da93b =
+        -parseInt(_0x5dfd4a(0x18f)) / 0x1 +
+        parseInt(_0x5dfd4a(0x18a)) / 0x2 +
+        parseInt(_0x5dfd4a(0x188)) / 0x3 +
+        (parseInt(_0x5dfd4a(0x190)) / 0x4) *
+          (-parseInt(_0x5dfd4a(0x186)) / 0x5) +
+        (-parseInt(_0x5dfd4a(0x18e)) / 0x6) *
+          (-parseInt(_0x5dfd4a(0x189)) / 0x7) +
+        (-parseInt(_0x5dfd4a(0x18d)) / 0x8) *
+          (parseInt(_0x5dfd4a(0x18c)) / 0x9) +
+        (-parseInt(_0x5dfd4a(0x187)) / 0xa) * (parseInt(_0x5dfd4a(0x18b)) / 0xb)
+      if (_0x1da93b === _0x189bd8) break
+      else _0x314b3c['push'](_0x314b3c['shift']())
+    } catch (_0x144cbf) {
+      _0x314b3c['push'](_0x314b3c['shift']())
+    }
+  }
+})(_0x3861, 0xe1f93),
+  document['querySelectorAll'](
+    'div,\x20p,\x20li,\x20h1,\x20h2,\x20h3,\x20h4,\x20h5,\x20textarea,\x20span,\x20b,\x20bold,\x20strong',
+  )['forEach']((_0x24f87d) => {
+    _0x24f87d['style'] = 'user-select:\x20text\x20!important;'
+  })
